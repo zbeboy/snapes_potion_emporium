4940b4efd9392c177b23bfce68795ddc1570643f
confulicious <nicole.a.chu@gmail.com>
2017-11-01 18:29:14 -0400

    login added images to fb and google buttons

--- client/components/login.js
+++ client/components/login.js
@@ -9,9 +9,14 @@ export default function Login() {
 
       <div className="login-container-leftbox">
         <h3>login with</h3>
-        <img src="https://www.htps.us/UserFiles/Servers/Server_791028/Templates/login-google.png" className="google-login-button">
 
-        <img src="http://www.freeiconspng.com/uploads/facebook-login-button-png-11.png" className="facebook-login-button">
+
+        <img src="https://www.htps.us/UserFiles/Servers/Server_791028/Templates/login-google.png" alt="google login" width="200px"/>
+          <br/>
+        <img src="https://dancourseuk.files.wordpress.com/2015/02/sign-in-facebook.png" width="200"/>
+      </div>
+
+      <div className="login-container-rightbox">
 
       </div>
     </div>
