341e576f8f6f13dca8f942d9fe13494c38340038
Sarah Katz <soshasarah@gmail.com>
2017-11-03 15:11:02 -0400

    completed single product page

---
+++ client/components/SingleProductPage.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {AllReviews} from './AllReviews'
+
+export class SingleProductPage extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const { addToCart, currentProduct } = props
+    return (
+      <div>
+          <Link>Back</Link> 
+          <h3>{currentProduct.name}</h3>
+          <img src={currentProduct.imageURL} />
+          <h6>${currentProduct.price}</h6>
+          <p>{currentProduct.description}</p>
+          <button onClick={addToCart}>Add Item To Cart</button>
+          <AllReviews />
+      </div>
+    )
+  }
+}
+
+// const mapState = (state) => {
+//   return {
+//     currentProduct: state.currentProduct
+//   }
+// }
+
+// const mapDispatch = (dispatch) => {
+//   return {
+//     addToCart (id) {
+//         dispatch(addToCart(id))
+//     }
+//   }
+// }
+
+// export const AllReviewsContainer = connect(mapState, mapDispatch)(AllReviews);
