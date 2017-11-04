2b3c9d4381762e573441202a0ecd84f546e11d2a
confulicious <nicole.a.chu@gmail.com>
2017-11-04 14:27:05 -0400

    finished skeleton of create login dummy

--- client/components/create-account.js
+++ client/components/create-account.js
@@ -7,6 +7,29 @@ const CreateAccount = () => {
   return (
     <div>
       Create Account
+      <form>
+          <label className="name">
+           Name:
+          <input className="name-field" />
+          </label>
+          <label>
+           Email:
+          <input className="email-field" />
+          </label>
+          <label>
+           Password:
+          <input className="password-field" />
+          </label>
+          <label>
+           Confirm Password:
+          <input className="confirm-password-field" />
+          </label>
+        <button>Create Account</button>
+      </form>
+
+      <p>Have an account? Return to <Link to="login"><span>Login Page</span></Link></p>
+
+
     </div>
   )
 
